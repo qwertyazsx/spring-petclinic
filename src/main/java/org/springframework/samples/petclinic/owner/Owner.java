2bf223b07c2cd459116d4e3fbf4c522db3ef83ab
박정현(Junghyun Park)/주문개발팀/11ST <qwertyazsx@sk.com>
2020-02-06 17:30:35 +0900

    2강 과제 3 | Owner에 age 추가

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -59,7 +59,11 @@ public class Owner extends Person {
 	@Digits(fraction = 0, integer = 10)
 	private String telephone;
 
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+	@Column(name = "age")
+    @Digits(fraction = 0, integer = 3)
+    private String age;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
 	private Set<Pet> pets;
 
 	public String getAddress() {
@@ -85,6 +89,14 @@ public class Owner extends Person {
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 
 	protected Set<Pet> getPetsInternal() {
 		if (this.pets == null) {
@@ -144,7 +156,9 @@ public class Owner extends Person {
 
 				.append("id", this.getId()).append("new", this.isNew()).append("lastName", this.getLastName())
 				.append("firstName", this.getFirstName()).append("address", this.address).append("city", this.city)
-				.append("telephone", this.telephone).toString();
-	}
+                .append("telephone", this.telephone)
+                .append("age", this.age)
+                .toString();
+    }
 
 }
